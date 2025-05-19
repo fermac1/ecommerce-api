d2d869635e8cc56c9ca7b74ae2655c9602d6f15c
fermac1 <pfermac@gmail.com>
2025-05-19 15:15:51 +0100

    fix: vercel.json

--- api/index.php
+++ api/index.php
@@ -18,9 +18,9 @@ require __DIR__ . '/../public/index.php';
 |
 */
 
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+// if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 |
 */
 
-require __DIR__ . '/../vendor/autoload.php';
+// require __DIR__ . '/../vendor/autoload.php';
 
 
 /*
@@ -47,12 +47,12 @@ require __DIR__ . '/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+// $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+// $kernel = $app->make(Kernel::class);
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+// $response = $kernel->handle(
+//     $request = Request::capture()
+// )->send();
 
-$kernel->terminate($request, $response);
+// $kernel->terminate($request, $response);
